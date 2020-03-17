9cbeb9f78899a9af3ff798398dc70861a4c3fcf0
Kelvin Chu <chubillkelvin@gmail.com>
2020-03-17 13:10:15 +0800

    Add simple UI to demo page.

--- app/module/main/component/Main.tsx
+++ app/module/main/component/Main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {StyleSheet, View, Text} from "react-native";
+import {StyleSheet, View, Text, SafeAreaView} from "react-native";
 import {connect, DispatchProp} from "react-redux";
 import {RootState} from "app/type/state";
 
@@ -11,9 +11,14 @@ class AppMain extends React.PureComponent<Props> {
     render() {
         // TODO: RootRouter, ContextProvider, GlobalLoadingIndicator, OverlayManager
         return (
-            <View style={styles.container}>
-                <Text>core-native-boilerplate</Text>
-            </View>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.header}>
+                    <Text style={styles.text}>core-native-boilerplate</Text>
+                </View>
+                <View style={styles.content}>
+                    <Text style={styles.text}>Hello, World!</Text>
+                </View>
+            </SafeAreaView>
         );
     }
 }
@@ -27,5 +32,22 @@ export default connect(mapStateToProps)(AppMain);
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: "space-between",
+        alignItems: "stretch",
+        backgroundColor: "steelblue",
+    },
+    header: {
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    content: {
+        flex: 1,
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 24,
     },
 });
