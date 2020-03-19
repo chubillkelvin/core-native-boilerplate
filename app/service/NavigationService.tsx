ff01ea88adcc93b29112c99ef98b350f9ac06b73
Kelvin Chu <chubillkelvin@gmail.com>
2020-03-19 11:11:35 +0800

    Update NavigationService.

--- app/service/NavigationService.tsx
+++ app/service/NavigationService.tsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, {FC} from "react";
 import {HomeComponent} from "../module/common/home";
-import {NavigationContainer} from "@react-navigation/native";
+import {NavigationContainer, NavigationContainerRef, StackActions} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
+import {ParamListBase} from "@react-navigation/routers";
 
 const Stack = createStackNavigator();
 
-export const App = () => (
-    <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeComponent} options={{title: "core-native-boilerplate", headerTintColor: "white", headerStyle: {backgroundColor: "steelblue"}}} />
-        </Stack.Navigator>
-    </NavigationContainer>
-);
+const navigationRef = React.createRef<NavigationContainerRef>();
+
+export const navigate = (name: keyof ParamListBase, params: ParamListBase) => {
+    navigationRef.current?.navigate(name, params);
+};
+
+export const push = (name: keyof ParamListBase, params: ParamListBase) => {
+    navigationRef.current?.dispatch(StackActions.push(name, params));
+};
+
+export const App: FC = () => {
+    return (
+        <NavigationContainer ref={navigationRef}>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={HomeComponent}
+                    options={{
+                        title: "core-native-boilerplate",
+                        headerTintColor: "white",
+                        headerStyle: {backgroundColor: "steelblue"},
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
