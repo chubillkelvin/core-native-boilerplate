9c1c1bb850b777fb47f0bcfc6b16f51ab7d9f91e
Kelvin Chu <chubillkelvin@gmail.com>
2020-03-22 18:50:48 +0800

    Update node scripts to separate format and check scripts.

---
+++ node-script/format.js
@@ -0,0 +1,8 @@
+const {runProcess} = require("./utility");
+
+function format(path) {
+    runProcess("prettier", ["--config", "./prettier.json", "--write", path]);
+}
+
+format("./*.{js,jsx,ts,tsx,json}");
+format("./app/**/*.{js,jsx,ts,tsx,json}");
